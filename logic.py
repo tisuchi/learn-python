56c10ac99ac41b434735c2d8da8573412a245ec7
tisuchi <tisuchi@gmail.com>
2017-10-24 14:25:01 +0800

    learn logic and conditional expression

---
+++ logic.py
@@ -0,0 +1,20 @@
+#check logical part
+
+
+a, b = 1, 2
+
+if a < b:
+	print('a ({}) is less than b ({})'.format(a,b))
+else:
+	print('b ({}) is less than a ({})'.format(a,b))
+
+
+
+# conditional Expression
+#pseducode "Value that you want to print for First Variable" if var1 > var2 else "value for second variable"
+print("A is smallerthan B" if a < b else " A is gratter than B")
+
+
+
+
+
